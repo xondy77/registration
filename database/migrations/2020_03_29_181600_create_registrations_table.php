83e47f92103f1c49733d78a1ba89e23b16f6687f
Nixson Henry <henrynixon.hn@gmail.com>
2020-03-30 14:18:19 -0400

    first commit

---
+++ database/migrations/2020_03_29_181600_create_registrations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $tabe->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('phone');
+            $table->integer('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::resource('registrations', 'RegistrationsController');
+
+// Route::get('registrations', 'RegistrationsController@index');
+// Route::get('registrations/create', 'RegistrationsController@create');
+// Route::post('registrations', 'RegistrationsController@store');
+// Route::get('registrations/{registration}', 'RegistrationsController@show');
+// Route::get('registrations/{registration}/edit', 'RegistrationsController@edit');
+// Route::patch('registrations/{registration}', 'RegistrationsController@update');
+// Route::get('registrations/{registration}', 'RegistrationsController@destroy');
+
+
